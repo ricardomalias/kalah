0f532a6840063fdc4721f6ca4172d5fd35b185b3
Ricardo Malias <ricardomalias@gmail.com>
2022-03-07 03:42:08 -0300

    feat: add validation to create a game

--- src/main/java/com/game/kalah/dto/GameDTO.java
+++ src/main/java/com/game/kalah/dto/GameDTO.java
@@ -3,9 +3,14 @@ package com.game.kalah.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Builder
 public class GameDTO {
+    @NotBlank(message = "game.first_player_not_found")
     private String firstPlayerName;
+
+    @NotBlank(message = "game.second_player_not_found")
     private String secondPlayerName;
 }
